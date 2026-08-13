fed80500c6824fc61906c6ee1e1baaeece17efe9
hekinotreal <heki7249@gmail.com>
2026-08-14 00:31:00 +0300

    Add cache-busting version tags so updates show immediately

--- js/technical/loader.js
+++ js/technical/loader.js
@@ -2,7 +2,7 @@
 
 for (file in modInfo.modFiles) {
     let script = document.createElement("script");
-    script.setAttribute("src", "js/" + modInfo.modFiles[file]);
+    script.setAttribute("src", "js/" + modInfo.modFiles[file] + "?v=" + VERSION.num);
     script.setAttribute("async", "false");
     document.head.insertBefore(script, document.getElementById("temp"));
 }
